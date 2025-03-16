c2db2c76cf124c4fa4f99a56ece64a45e178cff7
Tarek <tledrissi@gmail.com>
2025-03-16 04:36:45 +0200

    supabase disabled

--- components/sync/SyncBackground.tsx
+++ components/sync/SyncBackground.tsx
@@ -4,19 +4,20 @@
 import { useEffect } from 'react';
 
 const SyncBackground = () => {
-  useEffect(() => {
-    const syncInterval = setInterval(() => {
-      if (navigator.onLine) {
-        fetch('/api/sync', { method: 'POST' })
-          .then((res) => res.json())
-          .then((data) => console.log(data.message))
-          .catch((err) => console.error('Sync error:', err));
-      }
-    }, 300 * 60 * 1000); // Sync every 5 minutes seconds
+  // This component will run in the background and sync data with the server
+  // useEffect(() => {
+  //   const syncInterval = setInterval(() => {
+  //     if (navigator.onLine) {
+  //       fetch('/api/sync', { method: 'POST' })
+  //         .then((res) => res.json())
+  //         .then((data) => console.log(data.message))
+  //         .catch((err) => console.error('Sync error:', err));
+  //     }
+  //   }, 300 * 60 * 1000); // Sync every 5 minutes seconds
 
-    // Clean up the interval when the component unmounts
-    return () => clearInterval(syncInterval);
-  }, []);
+  //   // Clean up the interval when the component unmounts
+  //   return () => clearInterval(syncInterval);
+  // }, []);
 
   return null;
 };
